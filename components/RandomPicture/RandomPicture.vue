d9956ff1f7153b9bf6d987f0665365ffffe3a7dd
Charles Farquharson <hello@cfarq.co.uk>
2024-07-11 14:50:36 +0200

    remove comments

--- components/RandomPicture/RandomPicture.vue
+++ components/RandomPicture/RandomPicture.vue
@@ -12,7 +12,6 @@
         @click="fetchRandomImage"
         class="bg-green-500 flex hover:bg-green-400 transition relative py-4 px-4 rounded disabled:bg-gray-500 w-full md:max-w-[200px]"
       >
-        <!--  -->
         <div v-if="isDisabled" class="w-5 flex flex-col justify-center">
           <Loader />
         </div>
