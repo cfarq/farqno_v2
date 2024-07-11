a19ce751312327549677cd5be79a6b0ec907e729
Charles Farquharson <hello@cfarq.co.uk>
2024-07-11 15:39:20 +0200

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Charles Farquharson - Front-end Developer",
+    },
+  },
   compatibilityDate: "2024-04-03",
   css: ["~/assets/css/main.css"],
   postcss: {
