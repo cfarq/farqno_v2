b9b842240d52b709d0774c0398eeb62407f0d3c3
Charles Farquharson <hello@cfarq.co.uk>
2024-07-11 18:09:40 +0200

    another try

--- server/api/random-image.js
+++ server/api/random-image.js
@@ -1,18 +1,13 @@
 import { readdirSync } from "fs";
 import { join } from "path";
 import { getQuery } from "h3";
-import { useRuntimeConfig } from "nuxt/app";
 
 export default defineEventHandler((event) => {
   const query = getQuery(event);
   const imageDir = query.route;
   const currentImageUrl = query.currentImageUrl;
-  const config = useRuntimeConfig();
 
-  const imagesDir = join(
-    process.cwd(),
-    `${config.public.baseUrl}/images/${imageDir}`
-  );
+  const imagesDir = join(process.cwd(), `/__vercel/images/${imageDir}`);
   const files = readdirSync(imagesDir);
 
   const availableImages = files.filter((file) => {
